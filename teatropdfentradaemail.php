7f5f1940f82196beec137fac68ab5595d4be4c25
Javier Jarrin <javjarrin@gmail.com>
2019-12-11 19:40:34 +0100

    correo

--- teatropdfentradaemail.php
+++ teatropdfentradaemail.php
@@ -76,7 +76,7 @@
         $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
         $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
         $mail->Username   = 'javjarrin@gmail.com';                     // SMTP username
-        $mail->Password   = 'picopalo99';                               // SMTP password
+        $mail->Password   = '';                               // SMTP password
         $mail->SMTPSecure = 'tls';         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` also accepted
         $mail->Port       = 587;                                    // TCP port to connect to
 
